261145bc097e4b703142ec21fa09c68de1adb02d
Travis <mynameismyname1992@gmail.com>
2025-12-02 11:46:11 -0600

    Add background music and sfx

--- constants.py
+++ constants.py
@@ -1,25 +1,24 @@
-# Game
+# CONSTANTS
 WINDOW_WIDTH = 360
 WINDOW_HEIGHT = 640
-WINDOW_TITLE = "Flappy Bird"
 FPS = 60
 
-GRAVITY = 900 # pixels per second ^ 2 (positive = down)
-
-# Bird
-BIRD_X = WINDOW_WIDTH // 8
-BIRD_Y = WINDOW_HEIGHT // 2
+BIRD_X = WINDOW_WIDTH / 8
+BIRD_Y = WINDOW_HEIGHT / 2
 BIRD_WIDTH = 34
 BIRD_HEIGHT = 24
-FLAP_VELOCITY = -300 # negative = up, pixels/sec
+BIRD_VELOCITY_Y = 0 # move bird up/down speed
+FLAP_COOLDOWN = 120
+GRAVITY = 0.4
 
-# Pipe
 PIPE_X = WINDOW_WIDTH
 PIPE_Y = 0
 PIPE_WIDTH = 64
 PIPE_HEIGHT = 512
-PIPE_SPEED = 150
-PIPE_GAP = 150
-PIPE_INTERVAL = 1.6
+PIPE_VELOCITY_X = -2
 
-GROUND_Y = 0
+CRASH_SOUND = 'assets/sounds/sfx_hit.wav'
+DIE_SOUND = 'assets/sounds/sfx_die.wav'
+POINT_SOUND = 'assets/sounds/sfx_point.wav'
+FLAP_SOUND = 'assets/sounds/sfx_swooshing.wav'
+BG_MUSIC = 'assets/sounds/8-bit-arcade-138828.mp3'
--- main.py
+++ main.py
@@ -1,33 +1,171 @@
+from time import sleep
 import pygame
 from sys import exit
+import random
 from constants import *
 
-# Initialize
+# Initialize Pygame and screen
 pygame.init()
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Flappy Bird")
 clock = pygame.time.Clock()
 
-game_state = "START"
+# SOUNDS
+pygame.mixer.music.load(BG_MUSIC)
+pygame.mixer.music.play(-1)
+pygame.mixer.music.set_volume(0.05)
+
+pygame.mixer.set_num_channels(32)
+
+flap_sound = pygame.mixer.Sound(FLAP_SOUND)
+flap_sound.set_volume(0.1)
+die_sound = pygame.mixer.Sound(DIE_SOUND)
+die_sound.set_volume(0.1)
+point_sound = pygame.mixer.Sound(POINT_SOUND)
+point_sound.set_volume(0.1)
+
+last_flap_time = 0
+
+# Pipe Spawning Timer
+create_pipes_timer = pygame.USEREVENT + 0
+pygame.time.set_timer(create_pipes_timer, 1500) # every 1.5 seconds
+
+
+# IMAGES
+BG = pygame.image.load("assets/flappybirdbg.png")
+BIRD_IMG = pygame.image.load("assets/flappybird.png")
+BIRD_IMG = pygame.transform.scale(BIRD_IMG, (BIRD_WIDTH, BIRD_HEIGHT)).convert_alpha()
+TOP_PIPE_IMG = pygame.image.load("assets/toppipe.png")
+TOP_PIPE_IMG = pygame.transform.scale(TOP_PIPE_IMG, (PIPE_WIDTH, PIPE_HEIGHT)).convert_alpha()
+BOTTOM_PIPE_IMG = pygame.image.load("assets/bottompipe.png")
+BOTTOM_PIPE_IMG = pygame.transform.scale(BOTTOM_PIPE_IMG, (PIPE_WIDTH, PIPE_HEIGHT)).convert_alpha()
+
+
+class Bird(pygame.Rect):
+    def __init__(self, img):
+        pygame.Rect.__init__(self, BIRD_X, BIRD_Y, BIRD_WIDTH, BIRD_HEIGHT)
+        self.img = img
+
+
+class Pipe(pygame.Rect):
+    def __init__(self, img):
+        pygame.Rect.__init__(self, PIPE_X, PIPE_Y, PIPE_WIDTH, PIPE_HEIGHT)
+        self.img = img
+        self.passed = False
+
+
+# Game logic
+bird = Bird(BIRD_IMG)
+pipes = []
 score = 0
+game_over = False
+
+
+def draw():
+    screen.blit(BG, (0,0))
+    screen.blit(BIRD_IMG, bird)
+
+    for pipe in pipes:
+        screen.blit(pipe.img, pipe)
+
+    text_str = str(int(score))
+    if game_over:
+        text_str = f"Game Over: {text_str}"
+
+    text_font = pygame.font.SysFont("Liberation Mono", 40)
+    text_render = text_font.render(text_str, True, "white")
+    screen.blit(text_render, (10, 0))
+
+
+def move():
+    global BIRD_VELOCITY_Y, score, game_over
+    BIRD_VELOCITY_Y += GRAVITY
+    bird.y += BIRD_VELOCITY_Y
+
+    if bird.top < 0:
+        bird.top = 0
+        die_sound.play()
+        game_over = True
+        return
+    if bird.bottom > WINDOW_HEIGHT:
+        bird.bottom = WINDOW_HEIGHT
+        die_sound.play()
+        game_over = True
+        return
 
 
 
-# Game Loop
+    for pipe in pipes:
+        pipe.x += PIPE_VELOCITY_X
+
+        if not pipe.passed and bird.x > pipe.x + pipe.width:
+            score += 0.5
+            pipe.passed = True
+            point_sound.play()
+
+        if bird.colliderect(pipe):
+            game_over = True
+            die_sound.play()
+            return
+
+    # Clean up old pipes
+    while len(pipes) > 0 and pipes[0].x < -PIPE_WIDTH:
+        pipes.pop(0)
+
+
+
+def create_pipes():
+    random_pipe_y = PIPE_Y - PIPE_HEIGHT / 4 - random.random() * (PIPE_HEIGHT / 2)
+    
+    # Gap between pipes
+    gap_y = WINDOW_HEIGHT / 4
+    
+    # Top pipe
+    top_pipe = Pipe(TOP_PIPE_IMG)
+    top_pipe.y = random_pipe_y
+    pipes.append(top_pipe)
+    # Bottom pipe
+    bottom_pipe = Pipe(BOTTOM_PIPE_IMG)
+    bottom_pipe.y = top_pipe.y + top_pipe.height + gap_y
+    pipes.append(bottom_pipe)
+
+
+    print(len(pipes))
+
+
+
 while True:
-    # 60 fps
-    dt = clock.tick(FPS) / 1000.0
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        
-        # Handle keyboard/mouse input here
+
+        if event.type == create_pipes_timer and not game_over:
+            create_pipes()
+
+        # Add keyboard/mouse input here
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            BIRD_VELOCITY_Y = -6
+            current_time = pygame.time.get_ticks()
+            if current_time - last_flap_time >= FLAP_COOLDOWN:
+                last_flap_time = current_time
+                flap_sound.play()
+
+                # Press space to restart game
+                if game_over:
+                    bird.y = BIRD_Y
+                    pipes.clear()
+                    score = 0
+                    game_over = False
+                    pygame.mixer.music.rewind()
+                
 
 
-    # if game_state == "PLAYING":
-    #     update_game(dt)
+    if not game_over:
+        # Move pipes to the left
+        move()
 
-
-    
-    # draw()
+        draw()
+        pygame.display.update()
+        clock.tick(FPS)
